253d4cf2b1ed6ecc4cd25a155a2f395ca8beae78
iqHarishtalluru <88470146+Harshatalluru@users.noreply.github.com>
2024-01-28 20:15:53 +0530

    first commit

---
+++ fn-mafia/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Landingpage from './assign/landingpage';
+
+function App() {
+  return (
+    <div className="">
+      <Landingpage/>
+    </div>
+  );
+}
+
+export default App;
